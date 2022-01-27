abb315ce83932fab04cf06477c28b737cef8a487
soduma <soduma@naver.com>
2022-01-27 16:18:50 +0900

    Initial Commit

---
+++ News_MVPUITests/News_MVPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  News_MVPUITestsLaunchTests.swift
+//  News_MVPUITests
+//
+//  Created by 장기화 on 2022/01/27.
+//
+
+import XCTest
+
+class News_MVPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
